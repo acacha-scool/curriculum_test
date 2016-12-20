8459c6bd23fc9ac7f60434a8148eefdabf73c3d4
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-12-20 18:01:43 +0100

    Sync

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -31,11 +31,6 @@
                                 {{ session('message') }}
                             </div>
                         @endif
-                        {{--<div class="alert alert-danger alert-dismissible">--}}
-                            {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>--}}
-                            {{--<h4><i class="icon fa fa-check"></i> Oops!</h4>--}}
-                            {{--Something is wrong!--}}
-                        {{--</div>--}}
 
                         <form role="form" action="/studies" method="post" id="createStudy">
                             <!-- text input -->
