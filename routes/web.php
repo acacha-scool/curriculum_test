e8071b5cf6ca8192f1c0c2c77df26d6f5830bdfb
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-12-21 16:28:06 +0100

    Sync

--- routes/web.php
+++ routes/web.php
@@ -24,24 +24,6 @@ Route::group(['middleware' => 'auth'], function () {
         return view('tokens');
     });
     #adminlte_routes
-    Route::get('contact', 'ContactController@index')->name('contact');
-
-    Route::get('about', 'AboutController@index')->name('about');
-
-    Route::get('course2', 'Course2Controller@index')->name('course2');
-
-    Route::get('course1', function ()    {
-        $data = [];
-        return view('course1',$data);
-    })->name('course1');
-
-    Route::get('course', function ()    {
-        $data = [];
-        return view('course',$data);
-    })->name('course');
-
-
-//    Route::resource('studies', 'StudiesController');
 
 });
 
